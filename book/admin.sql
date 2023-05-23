ea6890bb327135a4365066b97f4ba0a664ed29f3
ElmanEhmedzade <131439483+ElmanEhmedzade@users.noreply.github.com>
2023-05-23 13:48:50 +0400

    boook

--- book/admin.sql
+++ book/admin.sql
@@ -1,26 +1,4 @@
-/*
-.
 
-Kitap ekleme i?lemi için bir "Kitap Ekle" prosedürü olu?turun. Bu prosedür, kitap bilgilerini alacak ve "Kitaplar" tablosuna ekleyecektir.
-Yazar ekleme i?lemi için bir "Yazar Ekle" prosedürü olu?turun. Bu prosedür, yazar bilgilerini alacak ve "Yazarlar" tablosuna ekleyecektir.
-Sat?c? ekleme i?lemi için bir "Sat?c? Ekle" prosedürü olu?turun. Bu prosedür, sat?c? bilgilerini alacak ve "Sat?c?lar" tablosuna ekleyecektir.
-Kitap sat??? gerçekle?tirme i?lemi için bir "Sat?? Gerçekle?tir" prosedürü olu?turun. Bu prosedür, kitap, mü?teri ve sat?c? bilgilerini alacak, "Sat??lar" tablosuna sat?? bilgilerini ekleyecek ve ilgili tablolarda güncellemeler yapacakt?r.
-PL/SQL Fonksiyonlar? Haz?rlama:
-
-Belirli bir kitab?n sat?? miktar?n? hesaplayan bir "Sat?? Miktar?n? Hesapla" fonksiyonu olu?turun. Bu fonksiyon, kitap kimlik numaras?n? alacak ve ilgili kitap için gerçekle?en toplam sat?? miktar?n? döndürecektir.
-Bir yazar?n yazd??? kitaplar?n say?s?n? hesaplayan bir "Kitap Say?s?n? Hesapla" fonksiyonu olu?turun. Bu fonksiyon, yazar kimlik numaras?n? alacak ve ilgili yazar?n kaç kitap yazd???n? döndürecektir.
-PL/SQL ??lemlerini ve Fonksiyonlar? Kullanma:
-
-Kitap eklemek için "Kitap Ekle"
-PL/SQL ??lemlerini ve Fonksiyonlar? Kullanma:
-
-Kitap eklemek için "Kitap Ekle" prosedürünü ça??rabilirsiniz:
-?ste?e Ba?l?: Di?er Fonksiyonlar? ve ??lemleri Ekleyin:
-
-"Sat?? Miktar?n? Hesapla" fonksiyonunu kullanarak belirli bir kitab?n sat?? miktar?n? alabilirsiniz:
-*/
-
---cedvel yaradaq
 --- 
 DROP TABLE BOOKS;
 CREATE TABLE books(
